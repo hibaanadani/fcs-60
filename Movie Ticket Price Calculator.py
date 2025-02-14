353c1ea42ef2b6054cc2fc55b26b4342909b32ee
hibaanadani <hibaanadani1@gmail.com>
2025-02-14 18:39:44 +0200

    added more if conditions

--- Movie Ticket Price Calculator.py
+++ Movie Ticket Price Calculator.py
@@ -1,6 +1,10 @@
 #Exercise 1:
 age= input("Enter Your Age: ")
-if int(age) <=10:
+if int(age) <=5:
     print("The ticket price is $5. ")
+elif int(age) <=10:
+    print("The ticket price is $7. ")
+elif int(age) <=15:
+    print("The ticket price is $10. ")
 else:
-    print("The ticket price is $10.")
+    print("The ticket price is $12.")
